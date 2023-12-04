68f98d2490a8b4d9457bcdcbcc903131104a795d
caitz1310 <142206866+caitz1310@users.noreply.github.com>
2023-12-04 22:00:31 +0200

    Add files via upload

---
+++ src/main/java/com/mycompany/urma_caitlin_hermanus_20232198/URMA_Caitlin_Hermanus_20232198.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+package com.mycompany.urma_caitlin_hermanus_20232198;
+
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Caitlin
+ */
+public class URMA_Caitlin_Hermanus_20232198 {
+
+    public static void main(String[] args) throws SQLException {
+        //open welcome frame
+        Welcome welcome = new Welcome();
+        welcome.setVisible(true);
+    }
+}
